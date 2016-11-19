9da4ba0903e1039841fa93d915063a2e0b896df4
Peter Henry <me@peterhenry.net>
2016-11-19 01:57:41 -0600

    completed receiving actions from the server

--- src/index.jsx
+++ src/index.jsx
@@ -7,22 +7,15 @@ import io from 'socket.io-client';
 
 import reducer from './reducer';
 import App from './components/App';
-import VotingContainer from './components/Voting';
-import ResultsContainer from './components/Results';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 
-const store = createStore(reducer);
-
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: ['Sunshine', '28 Days Later'],
-            tally: {'Sunshine': 2}
-        }
-    }
-});
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => {
+    store.dispatch({type: 'SET_STATE', state})
+});
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer} />
